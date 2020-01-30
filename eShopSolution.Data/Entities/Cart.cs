832127336d4c9608fd7b56aa10003416d01de6e1
admin <dong.orc12@gmail.com>
2020-01-30 07:12:03 +0700

    Add Entity

---
+++ eShopSolution.Data/Entities/Cart.cs
@@ -0,0 +1,10 @@
+﻿namespace eShopSolution.Data.Entities
+{
+    public class Cart
+    {
+        public int Id { set; get; }
+        public int ProductId { set; get; }
+        public int Quantity { set; get; }
+        public decimal Price { set; get; }
+    }
+}
---
+++ eShopSolution.Data/Entities/ProductInCategory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShopSolution.Data.Entities
+{
+    public class ProductInCategory
+    {
+        public Product Product { get; set; }
+
+        public Category Category { get; set; }
+    }
+}
