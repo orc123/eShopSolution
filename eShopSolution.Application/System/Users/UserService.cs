c3fe4de0f98b19f185d310f0d98244068bb54d81
admin <dong.orc12@gmail.com>
2020-03-19 13:11:43 +0700

    Login jwt

---
+++ eShopSolution.Application/System/Users/UserService.cs
@@ -0,0 +1,85 @@
+﻿using eShopSolution.Data.Entities;
+using eShopSolution.ViewModels.System.Users;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eShopSolution.Application.System.Users
+{
+    public class UserService : IUserService
+    {
+        private readonly UserManager<AppUser> _userManage;
+        private readonly SignInManager<AppUser> _signInManager;
+        private readonly RoleManager<AppRole> _roleManager;
+        private readonly IConfiguration _config;
+
+        public UserService(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager, RoleManager<AppRole> roleManager, IConfiguration configuration)
+        {
+            _userManage = userManager;
+            _signInManager = signInManager;
+            _roleManager = roleManager;
+            _config = configuration;
+        }
+
+        public async Task<string> Authencate(LoginRequest request)
+        {
+            var user = await _userManage.FindByNameAsync(request.UserName);
+            if (user == null)
+            {
+                return null;
+            }
+            var result = await _signInManager.PasswordSignInAsync(user, request.Password, request.RememberMe, true);
+            if (result.Succeeded)
+            {
+                var roles = await _userManage.GetRolesAsync(user);
+                var claims = new[]
+                {
+                    new Claim(ClaimTypes.Email, user.Email),
+                    new Claim(ClaimTypes.GivenName, user.FirstName),
+                    new Claim(ClaimTypes.Role,string.Join(";", roles))
+                };
+                var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Tokens:Key"]));
+                var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+                var token = new JwtSecurityToken(_config["Tokens:Issuer"],
+                    _config["Tokens:Issuer"],
+                    claims,
+                    expires: DateTime.Now.AddHours(3),
+                    signingCredentials: creds);
+
+                return new JwtSecurityTokenHandler().WriteToken(token);
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public async Task<bool> Register(RegisterRequest request)
+        {
+            var user = new AppUser()
+            {
+                Dob = request.Dob,
+                Email = request.Email,
+                FirstName = request.FirstName,
+                LastName = request.LastName,
+                UserName = request.UserName,
+                PhoneNumber = request.PhoneNumber
+            };
+            var result = await _userManage.CreateAsync(user, request.Password);
+            if (result.Succeeded)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
