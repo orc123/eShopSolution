256d485470f29948401e0912bbba6ab417f92476
admin <dong.orc12@gmail.com>
2020-03-28 21:39:44 +0700

    Add Login Page

--- eShopSolution.BackendApi/Controllers/UsersController.cs
+++ eShopSolution.BackendApi/Controllers/UsersController.cs
@@ -19,7 +19,7 @@ namespace eShopSolution.BackendApi.Controllers
 
         [HttpPost("authenticate")]
         [AllowAnonymous]
-        public async Task<IActionResult> Authenticate([FromForm]LoginRequest request)
+        public async Task<IActionResult> Authenticate([FromBody]LoginRequest request)
         {
             if (!ModelState.IsValid)
             {
@@ -35,7 +35,7 @@ namespace eShopSolution.BackendApi.Controllers
 
         [HttpPost("register")]
         [AllowAnonymous]
-        public async Task<IActionResult> Register([FromForm]RegisterRequest request)
+        public async Task<IActionResult> Register([FromBody]RegisterRequest request)
         {
             if (!ModelState.IsValid)
             {
