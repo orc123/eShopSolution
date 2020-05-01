cdb49cebc6328c53064b6b13692604fce327bc31
admin <dong.orc12@gmail.com>
2020-05-01 19:41:41 +0700

    create messeage

--- eShopSolution.AdminApp/Controllers/UserController.cs
+++ eShopSolution.AdminApp/Controllers/UserController.cs
@@ -29,6 +29,10 @@ namespace eShopSolution.AdminApp.Controllers
             };
             var data = await _userApiClient.GetUserPaging(request);
             ViewBag.Keyword = keyword;
+            if (TempData["result"] != null)
+            {
+                ViewBag.SuccessMessage = TempData["result"];
+            }
             return View(data.ResultObject);
         }
 
@@ -55,6 +59,7 @@ namespace eShopSolution.AdminApp.Controllers
             var result = await _userApiClient.RegisterUser(request);
             if(result.IsSuccessed)
             {
+                TempData["result"] = "Thêm mới người dùng thành công.";
                 return RedirectToAction("Index");
             }
             ModelState.AddModelError("", result.Message);
@@ -88,6 +93,7 @@ namespace eShopSolution.AdminApp.Controllers
             var result = await _userApiClient.UpdateUser(request.Id, request);
             if (result.IsSuccessed)
             {
+                TempData["result"] = "Cập nhật người dùng thành công.";
                 return RedirectToAction("Index");
             }
             ModelState.AddModelError("", result.Message);
@@ -116,6 +122,7 @@ namespace eShopSolution.AdminApp.Controllers
             var result = await _userApiClient.Delete(request.Id);
             if (result.IsSuccessed)
             {
+                TempData["result"] = "Xóa người dùng thành công.";
                 return RedirectToAction("Index");
             }
             ModelState.AddModelError("", result.Message);
